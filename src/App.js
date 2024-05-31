f20a3c384c5e04e21d06dd307d7e7be0735548c2
c3-alexsergian <alexander.sergian@c3.ai>
2024-05-30 23:20:35 -0400

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-//import { AppProvider } from './context/AppContext';
+import { AppProvider } from './context/AppContext';
 import Budget from './components/Budget';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
